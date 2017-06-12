9e4afbd51c97dfdb9acc3f1f4d2f5552ad8664ec
Nicolai Vasquez <nrvasquez@gmail.com>
2017-06-12 09:05:53 +0100

    prepare static html nav bar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-          Hello world
+      <div className="App">
+        <nav className="nav">
+          <div className="nav-left">
+            <a className="nav-item" href="/">
+              Weather App
+            </a>
+          </div>
+
+          <div className="nav-center">
+            <a className="nav-item" href="https://github.com/nicolaivasquez/weather-app">
+              <span className="icon">
+                <i className="fa fa-github"></i>
+              </span>
+            </a>
+          </div>
+
+          <span className="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </nav>
+        <div className="container is-fluid">
+          Content goes here
+        </div>
       </div>
     );
   }
